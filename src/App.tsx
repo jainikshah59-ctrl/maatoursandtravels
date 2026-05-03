6e5e539a36161fca7e941196ffad436a16fe56f1
jainikshah59-ctrl <jainikshah59@gmail.com>
2026-05-03 20:15:27 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-]import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import {
   MapPin, Car, Plane, Church, Building2,
   Phone, MessageCircle, Mail, MapPinned, Check,
